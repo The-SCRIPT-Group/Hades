7e41ce4f6d9ec575630e6b944a164944693bd8b8
Akhil Narang <akhilnarang.1999@gmail.com>
2019-02-19 15:23:47 +0530

    QRStuff: cleanup pylint warnings
    Add the qr code to gitignore, it gets regenerated everytime obviously
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
+"""
+Flask application to accept some details, generate, display, and email a QR code to users
+"""
 import base64
+import os
 from flask import Flask, request
+from sendgrid.helpers.mail import Email, Content, Mail, Attachment
 import sendgrid
 import qrcode
 
+# pylint: disable=invalid-name
 app = Flask(__name__)
-
-from sendgrid.helpers.mail import *
-
+sg = sendgrid.SendGridAPIClient(apikey=os.getenv('SENDGRID_API_KEY', None))
 
 @app.route('/submit', methods=['POST'])
 def stuff():
+    """
+    Accept data from the form, generate, display, and email QR code to user
+    """
     name = request.form['name']
     email = request.form['email']
     roll_number = request.form['roll_number']
@@ -17,8 +24,6 @@ def stuff():
     img.save('qr.png')
     img_data = open('qr.png', 'rb').read()
     encoded = base64.b64encode(img_data).decode()
-
-    sg = sendgrid.SendGridAPIClient(apikey=os.getenv('SENDGRID_API_KEY', None))
 
     from_email = Email('script.mailus@gmail.com')
     to_email = Email(email)
@@ -38,10 +43,14 @@ def stuff():
     print(response.body)
     print(response.headers)
 
-    return 'Please save this QR Code. It has also been emailed to you.<br><img src="data:image/jpeg;base64, {}"/>'.format(encoded)
+    return 'Please save this QR Code. It has also been emailed to you.<br><img src=\
+            "data:image/jpeg;base64, {}"/>'.format(encoded)
 
 @app.route('/')
 def root():
+    """
+    Main endpoint. Display the form to the user.
+    """
     with open('form.html', 'r') as file:
         data = file.read()
     return data
