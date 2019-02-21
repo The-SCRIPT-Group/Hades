3a6e45af19d28ce123d7f56dcb0e38af0cebd607
Akhil Narang <akhilnarang.1999@gmail.com>
2019-02-21 19:18:28 +0530

    QRStuff: add in database code
    Untested
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- app.py
+++ app.py
@@ -3,20 +3,51 @@
 Flask application to accept some details, generate, display, and email a QR code to users
 """
 
-# pylint: disable=invalid-name
+# pylint: disable=invalid-name,too-few-public-methods,no-member
 
 import base64
 import os
-from flask import Flask, request
-from sendgrid.helpers.mail import Email, Content, Mail, Attachment
-import sendgrid
-import qrcode
 
-SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY', None)
+import qrcode
+import sendgrid
+from sendgrid.helpers.mail import Attachment, Content, Email, Mail
+
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+
+
 FROM_EMAIL = os.getenv('FROM_EMAIL', None)
+SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY', None)
 
 app = Flask(__name__, static_url_path='')
 sg = sendgrid.SendGridAPIClient(apikey=SENDGRID_API_KEY)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', None)
+
+db = SQLAlchemy(app)
+
+DEPARTMENTS = {'cse': 'Computer Science and Engineering',
+               'ece': 'Electronics and Communication Engineering',
+               'mech': 'Mechanical Engineering',
+               'civil': 'Civil Engineering',
+               'chem': 'Chemical Engineering'}
+
+
+class User(db.Model):
+    """
+    Database model class
+    """
+    __tablename__ = 'users'
+    techo_id = db.Column(db.Integer, primary_key=True)
+    roll_number = db.Column(db.Integer, unique=True)
+    name = db.Column(db.String(30))
+    email = db.Column(db.String(50), unique=True)
+    phone = db.Column(db.Integer, unique=True)
+    department = db.Column(db.String(50))
+    year = db.Column(db.Integer)
+
+    def __repr__(self):
+        pass
 
 
 @app.route('/submit', methods=['POST'])
@@ -49,6 +80,12 @@ def stuff():
     print(response.body)
     print(response.headers)
 
+    user = User(roll_number=request.form['roll_number'], name=request.form['name'],
+                email=request.form['email'], phone=request.form['phone'],
+                department=DEPARTMENTS[request.form['department']], year=request.form['year'])
+    db.session.add(user)
+    db.session.commit()
+
     return 'Please save this QR Code. It has also been emailed to you.<br><img src=\
             "data:image/png;base64, {}"/>'.format(encoded)
 
@@ -78,7 +115,7 @@ def generate_qr(form_data, techo_id):
             Department: {}\nYear: {}".format(form_data['name'], form_data['email'],
                                              form_data['roll_number'], techo_id,
                                              form_data['phone_number'],
-                                             form_data['department'],
+                                             DEPARTMENTS[form_data['department']],
                                              form_data['year']))
 
 
