2badfbbe0b0ea0fa35ffabd685dbb1a02281e2cf
Kshitish Deshpande <43273771+ksdfg@users.noreply.github.com>
2019-11-10 09:19:00 +0530

    Update csi.py

--- tsg_registration/models/csi.py
+++ tsg_registration/models/csi.py
@@ -16,8 +16,6 @@ class CSINovember2019(db.Model):
     year = db.Column(db.String)
     csi_id = db.Column(db.String(3), unique=True)
 
-    params = ['name', 'email', 'phone', 'department', 'year', 'prn', 'csi_id']
-
     def __repr__(self):
         return "%r" % [
             self.id,
