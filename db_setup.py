7cca255a85718ecddfa16f0b1b5c111692a4c8c2
Akhil Narang <akhilnarang.1999@gmail.com>
2019-12-02 18:31:00 +0530

    QRStuff: add some utility scripts
      * These are used for creating new tables in the database, and insertion of rows into `events` table
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

---
+++ db_setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from tsg_registration import db
+from tsg_registration.models.event import Events
+
+db.create_all()
+
+for table in db.engine.table_names():
+    current_event = db.session.query(Events).get(table)
+    if current_event is None:
+        full_name = input(f"Enter full name for table {table}: ")
+        new_event = Events(name=table, full_name=full_name)
+        db.session.add(new_event)
+        db.session.commit()
+        print(f"Added event {table} with full name {full_name}")
+    else:
+        print(f"Found table {current_event.name} - {current_event.full_name}")
