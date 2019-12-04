5d0091d5f67b118de4897ade76918787638c4ad2
Akhil Narang <akhilnarang.1999@gmail.com>
2019-12-04 14:44:17 +0530

    Hades: add script to reset password
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

---
+++ reset_password.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from sys import exit
+from getpass import getpass
+
+from sqlalchemy.exc import IntegrityError
+
+from hades import app, bcrypt, db
+from hades.models.event import Events
+from hades.models.user import Users
+from hades.models.user_access import Access
+
+username = input("Enter username: ")
+
+user = db.session.query(Users).get(username)
+if user is None:
+    print(f"User {username} does not exist!")
+    exit(1)
+
+print("Enter new password: ", end="")
+user.password = bcrypt.generate_password_hash(getpass()).decode("utf-8")
+try:
+    db.session.commit()
+except Exception as e:
+    print("Exception occurred!")
+    print(e)
+    db.session.rollback()
