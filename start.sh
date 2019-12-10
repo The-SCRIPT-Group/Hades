b28fc6160d34df6220ea92ed10e6c7b2919c3bbf
Akhil Narang <akhilnarang.1999@gmail.com>
2019-12-10 15:13:41 +0530

    Hades: drop unused variable
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@
 [[ -d "venv" ]] || python3.8 -m venv ./venv
 source venv/bin/activate
 source .env
-export AUTHORIZATION_TOKEN BOT_API_KEY DATABASE_URL FROM_EMAIL GROUP_ID NOTIFY_ID SECRET_KEY SENDGRID_API_KEY
+export AUTHORIZATION_TOKEN BOT_API_KEY DATABASE_URL FROM_EMAIL GROUP_ID SECRET_KEY SENDGRID_API_KEY
 pip install -r requirements.txt
 gunicorn hades:app -b :5500
