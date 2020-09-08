ea3fb13d54619bc24855e558e44c1e451e58b2dd
ksdfg <ksdfg123@gmail.com>
2020-09-08 18:02:58 +0530

    change BOV to use mongoengine

--- hades/models/codex.py
+++ hades/models/codex.py
@@ -1,3 +1,5 @@
+from mongoengine import ValidationError, DynamicDocument, IntField, StringField
+
 from hades import db
 
 from requests import get
@@ -88,18 +90,34 @@ class CodexDecember2019(ValidateMixin, db.Model):
         return super().validate()
 
 
-class BOV2020(ValidateMixin, db.Model):
+class BOV2020(DynamicDocument):
     """
     Database model class
     """
 
-    __tablename__ = 'bov_2020'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(31))
-    email = db.Column(db.String(51), unique=True)
-    phone = db.Column(db.String(19))
-    hackerrank_username = db.Column(db.String(50), unique=True)
-    country = db.Column(db.String(24))
+    @staticmethod
+    def __validate_hackerrank_username__(username):
+        """
+        Validate if hackerrank username exists
+        :param username: hackerrank username to test
+        :return: None, raise error if username doesn't exist
+        """
+        if (
+            get(f'https://hackerrank.com/{username}').content.decode().count(username)
+            < 3
+        ):
+            raise ValidationError("Your hackerrank profile doesn't seem to exist!")
+
+    meta = {'collection': 'bov_2020'}
+
+    id = IntField(primary_key=True)
+    name = StringField()
+    email = StringField(unique=True)
+    phone = StringField()
+    hackerrank_username = StringField(
+        unique=True, validation=__validate_hackerrank_username__
+    )
+    country = StringField()
 
     def __repr__(self):
         return '%r' % [
@@ -110,18 +128,3 @@ class BOV2020(ValidateMixin, db.Model):
             self.hackerrank_username,
             self.country,
         ]
-
-    def validate(self):
-        if (
-            get(f'https://hackerrank.com/{self.hackerrank_username}')
-            .content.decode()
-            .count(self.hackerrank_username)
-            < 3
-        ):
-            return f"Your hackerrank profile doesn't seem to exist!"
-
-        if self.query.filter(
-            BOV2020.hackerrank_username == self.hackerrank_username
-        ).first():
-            return f"Someone has already registered with hackerrank username <code>{self.hackerrank_username}</code>.<br/>Kindly contact the team if that is your username and it wasn't your registration"
-        return super().validate()
