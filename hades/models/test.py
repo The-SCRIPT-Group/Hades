4c030471cda0aa9dba9fe60e69e6c4a6657e6ddd
ksdfg <ksdfg123@gmail.com>
2020-09-09 17:39:01 +0530

    shift all models in test.py to mongo

--- hades/models/test.py
+++ hades/models/test.py
@@ -1,16 +1,17 @@
-from hades import db
+from mongoengine import DynamicDocument, IntField, StringField
 
 
-class TestTable(db.Model):
+class TestTable(DynamicDocument):
     """
     Database model class
     """
 
-    __tablename__ = 'test_users'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30))
-    email = db.Column(db.String(50), unique=True)
-    phone = db.Column(db.String(21), unique=True)
+    meta = {'collection': 'test_users'}
+
+    id = IntField(primary_key=True)
+    name = StringField()
+    email = StringField(unique=True)
+    phone = StringField(unique=True)
 
     def __repr__(self):
         return '%r' % [self.id, self.name, self.email, self.phone]
